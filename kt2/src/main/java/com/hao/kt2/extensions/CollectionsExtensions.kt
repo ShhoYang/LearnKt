1005fd86fb80fa95715d415f5df324e7b24d1c9f
hao <haoyshi@163.com>
2018-05-03 15:48:53 +0800

    Kotlin学习

---
+++ kt2/src/main/java/com/hao/kt2/extensions/CollectionsExtensions.kt
@@ -0,0 +1,20 @@
+package com.hao.kt2.extensions
+
+/**
+ * @author Yang Shihao
+ * @date 2018/5/3
+ */
+
+fun <K, V : Any> Map<K, V?>.toVarargArray(): Array<out Pair<K, V>> =
+        map({ Pair(it.key, it.value!!) }).toTypedArray()
+
+inline fun <T, R : Any> Iterable<T>.firstResult(predicate: (T) -> R?): R {
+    for (element in this) {
+        val result = predicate(element)
+        if (result != null) {
+            return result
+        }
+    }
+
+    throw NoSuchElementException("No element matching predicate was found")
+}
